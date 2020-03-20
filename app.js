46ff5d320856fa7d7a0241d7b15697bcc848fee0
yashguptaz <yashguptaofficial@gmail.com>
2020-03-20 19:37:19 +0530

    Corona Website

---
+++ app.js
@@ -0,0 +1,37 @@
+const output = document.querySelector('#output');
+output.innerHTML = `Loading...`;
+let html;
+
+fetch('https://api.covid19api.com/summary')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.Countries[73]);
+    const {
+      NewConfirmed,
+      TotalConfirmed,
+      NewDeaths,
+      TotalDeaths,
+      NewRecovered,
+      TotalRecovered
+    } = data.Countries[73];
+
+    html = `
+    <p>
+      😷 Total Confirmed Cases:<span class="stat"> ${TotalConfirmed}</span> <br />
+      🆕 New Cases: <span class="stat">${NewConfirmed}</span> <br /><br />
+    </p>
+    
+    <p>
+      💀 Total Deaths: <span class="stat">${TotalDeaths}</span> <br />
+      🆕 New Deaths:<span class="stat"> ${NewDeaths}</span> <br /><br />
+    </p>
+
+    <p>
+      👨‍⚕️ Total Recovered: <span class="stat">${TotalRecovered}</span> <br />
+      🆕 New Recovered:<span class="stat"> ${NewRecovered} </span>
+    </p>
+
+    `;
+
+    output.innerHTML = html;
+  });
